3c5eb7da734162e72fa258dc018f4bd3e24033ef
progmamun <mamun333089@gmail.com>
2023-10-20 00:06:29 +0600

    edit

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,16 @@
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import routes from './app/routes';
-import cookieParser from 'cookie-parser';
-import { NotFoundHandler } from './errors/NotFoundHandler';
-import fileUpload from 'express-fileupload';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import router from "./routes/route";
 
 const app: Application = express();
 
-app.use(
-  cors({
-    origin: '*',
-    credentials: true,
-  }),
-);
-app.use(cookieParser());
-
-//parser
-app.use(fileUpload());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", router);
 
-// routes
-app.use('/api/v1', routes);
-
-// root route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Booking Management server is running...!');
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  res.send("server is running...");
 });
-
-//global error handler
-app.use(globalErrorHandler);
-
-//handle not found
-app.use(NotFoundHandler.handle);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,10 @@
-import { Server } from 'http';
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config/index';
-
-process.on('uncaughtException', error => {
-  console.error(error);
-  process.exit(1);
-});
-
-let server: Server;
+import app from "./app";
+import config from "./config";
 
 async function bootstrap() {
-  try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`🛢 Database is connected successfully`);
-
-    server = app.listen(config.port, () => {
-      console.log(`Application  listening on port ${config.port}`);
-    });
-  } catch (err) {
-    console.error('Failed to connect database', err);
-  }
-
-  process.on('unhandledRejection', error => {
-    if (server) {
-      server.close(() => {
-        console.error(error);
-        process.exit(1);
-      });
-    } else {
-      process.exit(1);
-    }
+  app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port} `);
   });
 }
 
 bootstrap();
-
-process.on('SIGTERM', () => {
-  console.log('SIGTERM is received');
-  if (server) {
-    server.close();
-  }
-});
